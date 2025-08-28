3d6fdc50cb1eba1db74a86c0ca07c9dad0f6a366
AayushiJaiswal <angleaayushi21@gmail.com>
2025-08-28 16:33:25 +0530

    Fixed Environment variable case

--- Server/db.js
+++ Server/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const Connection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://aayushi:MySecurePass123@cluster0.jy1gs.mongodb.net/votingS?retryWrites=true&w=majority&appName=Cluster0", {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
